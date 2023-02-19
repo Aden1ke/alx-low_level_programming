ff89b5624bfa5375d12861c540e618900b6d1ef1
Aden1ke <sarahogunlalu@gmail.com>
2023-02-19 12:26:54 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Write a program that prints the alphabet in lowercase.
+ * Return:0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = 'z'; ch <= 'z'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
