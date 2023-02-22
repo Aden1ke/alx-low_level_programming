4f3284889c7426fbe18c90ece2be6e7128d30646
Aden1ke <sarahogunlalu@gmail.com>
2023-02-22 08:10:04 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints _putchar.
+ * Return:0m(success)
+ */
+int main(void)
+{
+int letter;
+letter = '_';
+putchar(letter);
+letter = 'p';
+putchar(letter);
+letter = 'u';
+putchar(letter);
+letter = 't';
+putchar(letter);
+letter = 'c';
+putchar(letter);
+letter = 'h';
+putchar(letter);
+letter = 'a';
+putchar(letter);
+letter = 'r'
+putchar(letter);
+putchar("\n");
+return (0);
+}
