406ce6b84f0e014fe1f3583ecb28443e01fbbc5f
Aden1ke <sarahogunlalu@gmail.com>
2023-02-22 12:45:37 -0800

    5-sign.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
  /**
-  * _islower(int c) - Checks for lowercase character
-  * @C: The character to be checked
+  * _islower- Checks for lowercase character
+  * @C:The character to be checked
   * Return: 1 for lowerase character or 0 for anything else
   */
 int _islower(int c)
