6caeb7218cec9b5ad25244e43bdf1e19d913f188
Aden1ke <sarahogunlalu@gmail.com>
2023-02-23 05:24:08 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul- Checks for multiplication of two characters.
+ * @a:first digit.
+ * @b:second digit.
+ * Return: a * b.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
