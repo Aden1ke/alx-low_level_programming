c7d4eb2d629c9d555d6151894181848085c3d9f1
Aden1ke <sarahogunlalu@gmail.com>
2023-02-23 06:07:39 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,13 +5,12 @@
  */
 void more_numbers(void)
 {
-int x;
-int q;
-for (q = 0; q <= 10; q++)
+int q,x;
+for (q = 0; q < 10; q++)
 {
 for (x = 0; x <= 14; x++)
 {
-if (y > 9)
+if (x > 9)
 _putchar((x / 10) + '0');
 }
 _putchar((x % 10) + '0');
