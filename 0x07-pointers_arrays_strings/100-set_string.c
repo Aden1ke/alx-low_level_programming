530d1be49d0fe057ea6c23cc35a88ae79a7200ff
Aden1ke <sarahogunlalu@gmail.com>
2023-03-06 08:27:59 -0800

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "maiin.h"
+/**
+ * set_string - function definition
+ * @s: the pointer to use
+ * @to: the string value
+ * Description: set the value of a pointer to a char
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
