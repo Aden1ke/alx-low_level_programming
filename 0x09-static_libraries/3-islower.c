c163687d955c485145729379d8b4f79e2b2e769f
Aden1ke <sarahogunlalu@gmail.com>
2023-03-10 13:33:45 -0800

    0x09-static_libraries

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - tests c if lower or upper
+ * @c: character is passed as integer
+ * Description: c is the arguement as integer
+ * Return: void
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - function definition
+ * @n: integer that will be tested
+ * Description: convert and print number absolute
+ * Return: void
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else if (n > 0)
+{
+return (n);
+}
+else
+{
+return (n);
+}
+}
