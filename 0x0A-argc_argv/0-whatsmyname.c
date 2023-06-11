f20200add458f8dba145174b05fee1ec4e1e65ff
Aden1ke <sarahogunlalu@gmail.com>
2023-06-11 12:22:30 -0700

    source code

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: number of arguments.
+ * @argv: arguments inputed.
+ * Return: 0n success 1.
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
