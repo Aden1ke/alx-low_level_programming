e69ec300a65fe885001ffba9bb1cd95c64ff4e16
Aden1ke <sarahogunlalu@gmail.com>
2023-07-03 17:23:08 -0700

    free linked list node by node

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - function that frees a list_t list.
+ * @head: head of the linked list.
+ * Return: Always 0.
+ */
+void free_list(list_t *head)
+{
+	list_t *node = head;
+
+	while (head != NULL)
+	{
+		head = head->next;
+		free(node->str);
+		free(node);
+	}
+}
