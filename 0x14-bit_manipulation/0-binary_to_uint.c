2a5d392c9d93302996d74e53243742400aeeebbb
Aden1ke <sarahogunlalu@gmail.com>
2023-07-08 08:22:41 -0700

    betty binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,19 +9,19 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int decimal_val = 0;
-	
+
 	if (b == NULL)
 	{
 		return (0);
 	}
 	while (*b != '\0')
 	{
-	    if (*b != '0' && *b != '1')
-	{
-		return (0);
+		if (*b != '0' && *b != '1')
+		{
+			return (0);
+		}
+		decimal_val = (decimal_val << 1) + (*b - '0');
+		b++;
 	}
-	decimal_val = (decimal_val << 1) + (*b - '0');
-	b++;
-	}
-    return (decimal_val);
+	return (decimal_val);
 }
