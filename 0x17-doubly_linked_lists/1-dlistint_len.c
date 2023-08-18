8109972bce1d7170107787ee87ecac939bda7309
Aden1ke <sarahogunlalu@.com>
2023-08-18 22:16:02 +0100

    source codes

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * h: pointer to headlist
+ * Return: count
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+        }
+	return (count);
+}
