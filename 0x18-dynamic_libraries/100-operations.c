ebd2c9cc2a2acfd96b8a037af1aedd8f683352ae
Aden1ke <sarahogunlalu@gmail.com>
2023-08-21 10:50:43 -0700

    source code

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - function that adds.
+ * @a: 1st val
+ * @b: 2nd val
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - function that subtract.
+ * @a: 1st val
+ * Return: Always 0.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function that multiplies.
+ * @a: 1st val
+ * @b: 2nd val
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function that divides.
+ * @a: 1st val
+ * @b: 2nd val
+ * Return: Always 0.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - function that returns remainder of divisio 
+ * @a: 1st val
+ * @b: 2nd val
+ * Return: Always 0.
+ */
+int mod(int a, int b)
+{
+    return (a % b);
+}
